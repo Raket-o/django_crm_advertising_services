1568bed95038df7ce89511afbb58d328acd47ee6
Mikhail Sobolev <kseon@inbox.ru>
2024-04-17 02:06:10 +0400

    connect_css,static

--- django_crm/django_crm/settings.py
+++ django_crm/django_crm/settings.py
@@ -145,7 +145,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'django_crm', 'templates', 'static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'uploads'
